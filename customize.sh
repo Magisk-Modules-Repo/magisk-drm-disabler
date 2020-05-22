f50196a86664fdbaee9c3ce8235ba23488bf5918
Atrate <Atrate@protonmail.com>
2020-05-22 21:20:53 +0200

    Add initial keytest
    Signed-off-by: Atrate <Atrate@protonmail.com>

--- customize.sh
+++ customize.sh
@@ -1,3 +1,38 @@
+# Thanks to Jman420 for these volume input functions
+# --------------------------------------------------
+keytest() 
+{
+    ui_print "- Vol Key Test -"
+    ui_print "   Press Vol Up:"
+    (/system/bin/getevent -lc 1 2>&1 | /system/bin/grep VOLUME | /system/bin/grep " DOWN" > "$TMPDIR"/events) || return 1
+    return 0
+}
+
+choose() 
+{
+    #note from chainfire @xda-developers: getevent behaves weird when piped, and busybox grep likes that even less than toolbox/toybox grep
+    while (true); do
+        /system/bin/getevent -lc 1 2>&1 | /system/bin/grep VOLUME | /system/bin/grep " DOWN" > "$TMPDIR"/events
+        if (`cat "$TMPDIR"/events 2>/dev/null | /system/bin/grep VOLUME >/dev/null`); then
+            break
+        fi
+    done
+    if (`cat "$TMPDIR"/events 2>/dev/null | /system/bin/grep VOLUMEUP >/dev/null`); then
+        return 1
+    else
+        return 0
+    fi
+}
+
+# Check whether using a legacy device
+# -----------------------------------
+if keytest; then
+    FUNC=choose
+else
+    FUNC=false
+fi
+
+
 REPLACE="
 /system/app/LGDrm
 /system/etc/drm
